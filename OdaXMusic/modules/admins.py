037fc3e50a30844b5576427792e8255347dcde2e
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-07-01 01:46:00 +0700

    Update and rename DaisyXMusic/modules/admins.py to OdaXMusic/modules/admins.py

--- OdaXMusic/modules/admins.py
+++ OdaXMusic/modules/admins.py
@@ -16,15 +16,15 @@
 
 
 from asyncio.queues import QueueEmpty
-from DaisyXMusic.config import que
+from OdaXMusic.config import que
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from DaisyXMusic.function.admins import set
-from DaisyXMusic.helpers.channelmusic import get_chat_id
-from DaisyXMusic.helpers.decorators import authorized_users_only, errors
-from DaisyXMusic.helpers.filters import command, other_filters
-from DaisyXMusic.services.callsmusic import callsmusic
+from OdaXMusic.function.admins import set
+from OdaXMusic.helpers.channelmusic import get_chat_id
+from OdaXMusic.helpers.decorators import authorized_users_only, errors
+from OdaXMusic.helpers.filters import command, other_filters
+from OdaXMusic.services.callsmusic import callsmusic
 
 
 @Client.on_message(filters.command("adminreset"))
