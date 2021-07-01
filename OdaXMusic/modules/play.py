be8edad676b5b2ca131171d21f5f49e34780700b
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-07-01 11:55:13 +0700

    Update play.py

--- OdaXMusic/modules/play.py
+++ OdaXMusic/modules/play.py
@@ -516,7 +516,7 @@ async def play(_, message: Message):
     elif urls:
         query = toxt
         await lel.edit("🎵 **Processing**")
-        ydl_opts = {"format": "bestaudio[ext=mp3]"}
+        ydl_opts = {"format": "bestaudio[ext=m4a]"}
         try:
             results = YoutubeSearch(query, max_results=1).to_dict()
             url = f"https://youtube.com{results[0]['url_suffix']}"
