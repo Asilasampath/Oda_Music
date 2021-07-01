7af6abbbb043481fb07a5f3e9c5b9fa4ff285f0a
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-07-01 22:28:37 +0700

    Update msg.py

--- OdaXMusic/modules/msg.py
+++ OdaXMusic/modules/msg.py
@@ -18,10 +18,7 @@ from OdaXMusic.config import ASSISTANT_NAME, PROJECT_NAME
 
 
 class Messages:
-    START_MSG = await message.reply_sticker(
-        "CAACAgUAAxkBAAEOycBglaXxgN1VWNe8UMwECeyPxxhkDQACzQEAAvFQKFSmX8Ai5gisyh8E"
-    )
-    "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info."
+    START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info."
     HELP_MSG = [
         ".",
         f"""
